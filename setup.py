e6b826f122fa402dc9b85bfa82c6120379daae52
Karel Tutsu <KarelOmab@gmail.com>
2024-04-16 19:35:31 -0400

    drumpler_mammoth-2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drumpler_mammoth',
-    version='2.0.9',
+    version='2.1.0',
     author='Karel Tutsu',
     author_email='karel.tutsu@gmail.com',
     description='Framework for rapidly developing a restful API that requires post processing',
